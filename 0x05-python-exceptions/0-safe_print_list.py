c16b1e98f8068350b95875c0cc2845272f386aea
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-06-27 00:16:05 +0100

    fix pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end='')
             nb_print += 1
-        except:
+        except ValueError:
             break
     print()
     return nb_print
