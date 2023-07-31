3a0443843412bbf44ea099b851d7c04a402c99f9
MOUAD KHANOUCH <125469605+MD-MOUAD@users.noreply.github.com>
2023-07-31 23:14:53 +0100

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(magic=[]):
-    magic += ['BestSchool']
-    return ', '.join(magic)
--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-''' Locked class module'''
-
-
-class LockedClass:
-    '''Object that prevents dynamics attributes'''
-
-    __slots__ = ['first_name']
