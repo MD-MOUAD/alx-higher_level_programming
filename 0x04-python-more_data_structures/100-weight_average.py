b6b704a6a792a207df6c96105c26537a7e4ace19
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-09-01 23:30:46 +0100

    task 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
-
 def weight_average(my_list=[]):
     if my_list == []:
         return 0
-
-    prod = 0
-    res_sum = 0
-    for t in my_list:
-        t_list = list(t)
-        prod += t_list[0] * t_list[1]
-        res_sum += t_list[1]
-
-    return prod / res_sum
+    s_score = 0   # f_score = sum of score * weight
+    s_weight = 0  # s_weight = sum of weights
+    for tup in my_list:
+        s_score += tup[0] * tup[1]
+        s_weight += tup[1]
+    return s_score / s_weight
