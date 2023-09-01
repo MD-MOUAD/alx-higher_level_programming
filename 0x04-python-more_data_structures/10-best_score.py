f39fc2a072db7ad8705a66e96604b8966d33f3d2
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-09-01 21:40:03 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,17 +2,10 @@
 
 
 def best_score(a_dictionary):
-    max_v = None
-    key = None
-    if a_dictionary:
-        for k in a_dictionary:
-            if max_v is None:
-                max_v = a_dictionary[k]
-                key = k
-            elif a_dictionary[k] > max_v:
-                max_v = a_dictionary[k]
-                key = k
-
-        return key
-
-    return key
+    if a_dictionary == {} or a_dictionary is None:
+        return None
+    values = list(a_dictionary.values())
+    big = values[0]
+    for i in values:
+        big = i if i > big else big
+    return big
