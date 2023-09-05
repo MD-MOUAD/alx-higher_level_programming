e992da5650a7b982c0e72797ae28d8ca43bc888b
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-09-05 02:26:31 +0100

    fix raise mssg

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
             ValueError: If the value is negative.
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
