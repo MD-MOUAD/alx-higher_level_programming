db843e48c426222be043db29405f8fb51f05a68c
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-09-13 00:19:16 +0100

    python I/O tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-'''Module for task 0'''
+"""Module 0-read_file.
+Reads from a file and prints.
+"""
 
 
 def read_file(filename=""):
-    '''Read the contents of a file and print to stdout'''
-    with open(filename, 'r') as f:
-        print(f.read(), end="")
+    """Reads from filename and prints
+    its contents to stdout.
+
+    Args:
+        - filename: name of the file
+    """
+
+    with open(filename) as f:
+        read_text = f.read()
+        print(read_text, end="")
