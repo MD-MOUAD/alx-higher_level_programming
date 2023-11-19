bc50d6b75bb8608c90c16013d9c4d050b64ef76e
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-19 20:38:10 +0100

    1. Filter states

--- 0x0F-python-object_relational_mapping/0-select_states.py.session.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.py.session.sql
@@ -1,1 +1,9 @@
-SELECT * FROM `states`
+-- @block
+DROP TABLE states;
+-- @block
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Module for states"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+
+    curs = conn.cursor()
+
+    query = "SELECT `id`, `name` AS 'city'FROM `states`\
+        WHERE `name` LIKE 'N%' ORDER BY id ASC"
+    curs.execute(query)
+    query_rows = curs.fetchall()
+    for row in query_rows:
+        print(row)
+    curs.close()
+    conn.close()
