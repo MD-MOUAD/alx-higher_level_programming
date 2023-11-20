7207b76503bab2dc116dc8c966521546618cf850
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-20 01:12:56 +0100

    add asc order

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     query = "SELECT cities.id, cities.name, states.name\
         FROM `cities` INNER JOIN `states`\
         ON cities.state_id = states.id\
-        ORDER BY cities.id"
+        ORDER BY cities.id ASC"
     curs.execute(query)
     query_rows = curs.fetchall()
     for row in query_rows:
