5dc26c589a6a33f7f89e36415bda0e4c2f443340
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-11-25 00:34:12 +0100

    task 6

--- 0x0F-python-object_relational_mapping/0-select_states.py.session.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.py.session.sql
@@ -31,8 +31,8 @@ WHERE states.name LIKE BINARY 'Texas' ORDER BY cities.id ASC;
 -- Create states table in hbtn_0e_4_usa with some data
 CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
 USE hbtn_0e_4_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -50,3 +50,9 @@ INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
 INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
 INSERT INTO cities (state_id, name) VALUES (4, "New York");
 INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+
+-- @block
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+SHOW CREATE TABLE states;
