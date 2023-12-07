1c92b46b0d327a4b9cab4edd7fc2ed840d6acb32
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-07 20:53:50 +0100

    print nothing if the table is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
 
     # lists first State object from the database hbtn_0e_6_usa
     first = session.query(State).order_by(State.id).first()
-    if first:
-        print(f"{first.id}: {first.name}")
+    print(f"{first.id}: {first.name}") if first else print("Nothing")
 
     session.close()
