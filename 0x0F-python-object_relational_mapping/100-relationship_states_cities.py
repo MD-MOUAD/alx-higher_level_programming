91641c6b42e5915a40592c74458720542b977686
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-09 11:34:12 +0100

    base meatadata create all

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,12 +4,13 @@
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
 from relationship_city import City
-from relationship_state import State
 
 if __name__ == "__main__":
     db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
-    engine = create_engine(db_url, pool_pre_ping=True)
+    engine = create_engine(db_url, pool_pre_ping=True, echo=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
