81ecb058d2f44d1bbd6fae148069671106b8187b
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-12-09 11:43:50 +0100

    update : create tables if not exists

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -9,10 +9,7 @@ from model_city import City
 # from relationship_state import State
 
 if __name__ == "__main__":
-    engine = create_engine (
-        f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}",
-        pool_pre_ping=True,
-        echo=True
-    )
+    db_URL = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
+    engine = create_engine (db_URL, pool_pre_ping=True, echo=True)
 
     Base.metadata.create_all(engine)
