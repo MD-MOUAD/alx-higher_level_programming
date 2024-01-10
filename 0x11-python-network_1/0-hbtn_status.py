8f085f45cefd264db72a9f5adba034339e5c47eb
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-10 13:24:18 +0100

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-""" module doc """
-import urllib.request
+from urllib.request import urlopen
+"""What's my status?"""
 
-
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        utf_data = data.decode('utf-8')
-        resType = type(data)
-        print(f"Body response:\n\t- type: {resType}\n\t\
-- content: {data}\n\t- utf8 content: {utf_data}")
+if __name__ == '__main__':
+    with urlopen('http://madaralx.tech') as response:
+        body = response.read()
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode("utf8"))
