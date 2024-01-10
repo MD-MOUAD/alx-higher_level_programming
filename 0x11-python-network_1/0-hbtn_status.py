d9878e7791c6a023f6ef65aa5b478fa7cae771c1
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-10 13:39:40 +0100

    pycodestyle

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
     print(f"\t- type: {type(body)}\n\t- content: {body} \n\t\
-        - utf8 content: {body.decode('utf8')}")
+- utf8 content: {body.decode('utf8')}")
