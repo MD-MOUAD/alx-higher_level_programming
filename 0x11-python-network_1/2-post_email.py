f17e54bafbe8c961f33eae94aed69ee8258c25d6
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-01-10 14:12:54 +0100

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" module doc """
-import urllib.request
-import urllib.parse
-import sys
+"""module doc"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-    params = {"email": email}
-    utf_params = urllib.parse.urlencode(params).encode('utf-8')
-    req = urllib.request.Request(url, data=utf_params, method='POST')
-    with urllib.request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+if __name__ == '__main__':
+    url = argv[1]
+    data = {'email': argv[2]}
+    encoded_data = urlencode(data).encode('utf-8')
+    # create request object
+    request = Request(url, encoded_data, method='POST')
+
+    with urlopen(request) as response:
+        result = response.read()
+        print(result.decode('utf-8'))
